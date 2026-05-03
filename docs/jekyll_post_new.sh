a64da95dd36bf0bda0287181d3d8bcc1f72a2c96
tk3 <169007+tk3@users.noreply.github.com>
2026-05-03 21:57:21 +0900

    add

---
+++ docs/jekyll_post_new.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+post='_posts'
+
+today=`date +'%F'`
+title=$1
+
+if [ -z "$post" ] || [ -z "$today" ] || [ -z "$title" ]; then
+  echo "Error"
+  exit 1
+fi
+
+if [ -e "$post/$today-$title.md" ]; then
+  echo "Error: Already exists. file=$post/$today-$title.md"
+  exit 2
+fi
+
+post_date=`date +'%F %H:%M:%S %z'`
+
+cat << EOS > "$post/$today-$title.md"
+---
+layout: post
+title: "$1"
+date: $post_date
+categories:
+  - 
+tags:
+  - 
+---
+
+EOS
+
+$EDITOR "$post/$today-$title.md"
+
